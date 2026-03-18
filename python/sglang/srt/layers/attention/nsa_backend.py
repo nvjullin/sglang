0cc8fa97791f252e9ff5a3d7f44b97bbc518219a
Julien Lin <jullin@nvidia.com>
2026-03-18 08:37:24 +0000

    fixed None for cudagraph

--- python/sglang/srt/layers/attention/nsa_backend.py
+++ python/sglang/srt/layers/attention/nsa_backend.py
@@ -414,9 +414,9 @@ class NativeSparseAttnBackend(
         #   topk_indices_offset = [0, 0, 2, 2, 2, 5, 5, 5, 5]
         topk_indices_offset = None
 
-        # Centralized dispatch: decide all strategies for this batch
         self.set_nsa_impl(forward_batch)
-        if forward_batch.forward_mode.is_decode_or_idle():
+        # forward_batch is None only for cudagraph
+        if forward_batch is None or forward_batch.forward_mode.is_decode_or_idle():
             topk_transform_method = TopkTransformMethod.PAGED
         else:
             topk_transform_method = self.get_prefill_topk_transform_method()
@@ -2101,7 +2101,8 @@ class NativeSparseAttnBackend(
         else:
             self.use_mha = False  # Decode/verify always use MLA
 
-        if forward_batch.is_decode():
+        # forward_batch is None only for cudagraph
+        if forward_batch is None or forward_batch.is_decode_or_idle():
             # decode cannot be flashmla_auto
             assert self.nsa_decode_impl == "flashmla_kv"
             return
@@ -2150,7 +2151,8 @@ class NativeSparseAttnBackend(
             forward_batch.hisparse_coordinator is not None
             and forward_batch.forward_mode.is_decode_or_idle()
         )
-        if forward_batch.is_decode_or_idle():
+        # forward_batch is None only for cudagraph
+        if forward_batch is None or forward_batch.is_decode_or_idle():
             topk_transform_method = TopkTransformMethod.PAGED
         else:
             topk_transform_method = self.get_prefill_topk_transform_method()
